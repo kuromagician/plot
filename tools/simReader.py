c1b11b2981eee24d30baade96c6ef98f019812b2
Si Li <kuromagician@gmail.com>
2014-06-19 13:45:20 +0200

    add simulation reader

---
+++ tools/simReader.py
@@ -0,0 +1,53 @@
+'''
+Created on June 18, 2011
+
+@author: Si Li
+'''
+
+from functions import  *
+import sys
+import os
+import ctpDebugMsg
+
+filepath = "/home/nagatoyuki/Thesis/loglistener.txt"
+
+FILE_TYPE_CTPDEBUG = 0
+FILE_TYPE_CTPDATA = 1
+FILE_TYPE_ORWDEBUG = 2
+FILE_TYPE_ORWNT = 3
+
+def loadDebug(filepath):
+	if not os.path.isfile(filepath):
+		sys.exit("File does not exit!")
+	CtpDebugMsgs = []
+	CtpDataMsgs = []
+	OrwDebugMsgs = []
+	OrwNtMsgs = []
+	
+	with open(filepath, 'rb') as f:
+		lines = f.readlines()
+		for row in lines:
+			row = mysplit(row)
+			if len(row) == 7:
+				time = row[0].split(':')
+				if len(time) == 2:
+					timestamp = int(time[0])*60 + float(time[1])
+				else:
+					timestamp = int(time[0])*3600 + int(time[1])*60 + float(time[1])
+				#get the node ID
+				node = int((row[1].split(':'))[1])
+				filetype = int(row[2])
+				if filetype == FILE_TYPE_CTPDEBUG:
+					#debug msgs, get into dbg__a, dbg__b, msg.dbg__c
+					CtpDebugMsgs.append(ctpDebugMsg.SimDebugMsg(node, timestamp, int(row[3]), int(row[4]), int(row[5]), int(row[6])))
+				elif filetype == FILE_TYPE_CTPDATA:
+					CtpDataMsgs.append(ctpDebugMsg.SimDataMsg(node, timestamp, int(row[4]), int(row[5]), int(row[6])))
+				elif filetype == FILE_TYPE_ORWDEBUG:
+					OrwDebugMsgs.append(ctpDebugMsg.SimDebugMsg(node, timestamp, int(row[3]), int(row[4]), int(row[5]), int(row[6])))
+				elif filetype == FILE_TYPE_ORWNT:
+					OrwNtMsgs.append(ctpDebugMsg.SimNtMsg(node, timestamp, int(row[3]), int(row[4]), int(row[5]), int(row[6])))
+
+	return CtpDebugMsgs, CtpDataMsgs, OrwDebugMsgs, OrwNtMsgs
+				
+if __name__ == '__main__':
+	loadDebug(filepath)
